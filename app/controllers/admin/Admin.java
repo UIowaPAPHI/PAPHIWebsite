6fa5518b0e55e666c8f62d203d9942864811b4ac
UIowaPAPHI <pialphaphiuiowa@gmail.com>
2017-12-23 12:08:50 -0600

    master added login page

---
+++ app/controllers/admin/Admin.java
@@ -0,0 +1,17 @@
+package controllers.admin;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+
+public class Admin extends Controller {
+
+    public static void loginPage() {
+        render();
+    }
+
+}
